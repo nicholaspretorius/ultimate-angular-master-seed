5fe0da906241e6f47b6bebf96a1d21ce20c9def1
Nicholas Pretorius <nicholaspretorius@gmail.com>
2017-12-21 19:36:10 +0200

    Setting up Firebase for deployment

--- src/app/components/auth/auth.module.js
+++ src/app/components/auth/auth.module.js
@@ -2,4 +2,24 @@ angular
   .module('components.auth', [
     'ui.router',
     'firebase'
-  ]);
+  ])
+
+  .config(function ($firebaseRefProvider){
+    var config = {
+      apiKey: "AIzaSyAEu6h3kZ1d-Ec0iDPESMEAelZjdo-Rxz8",
+      authDomain: "contacts-manager-a4773.firebaseapp.com",
+      databaseURL: "https://contacts-manager-a4773.firebaseio.com",
+      projectId: "contacts-manager-a4773",
+      storageBucket: "contacts-manager-a4773.appspot.com",
+      messagingSenderId: "944334513855"
+    };
+
+    $firebaseRefProvider
+      .registerUrl({
+        default: config.databaseURL,
+        contacts: config.databaseURL + '/contacts'
+      });
+
+    firebase.initializeApp(config);
+  });
+  
